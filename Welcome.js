56971ba70ad09323da2908c834740c9085cc6231
vai0sx <130502134+vai0sx@users.noreply.github.com>
2023-06-06 14:26:16 -0600

    Create Welcome.js

---
+++ Welcome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Welcome = () => {
+  return (
+    <div>
+      <h1>Bienvenido a SonarLink</h1>
+      <p>Compre boletos para experiencias increíbles de manera segura y fácil.</p>
+    </div>
+  );
+};
+
+export default Welcome;
